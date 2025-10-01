82c51abfab479c4d0251d82634f4289bae8ca7ae
Ckangel <ckajeigbe1@gmail.com>
2025-10-01 21:27:01 +0000

    Added characters controller and routes

--- routes/characters.js
+++ routes/characters.js
@@ -1,1 +1,27 @@
 // Defines routes for character-related endpoints and maps them to controller functions.
+const express = require("express");
+const router = express.Router();
+const {
+  getAllCharacters,
+  getCharacterById,
+  createCharacter,
+  updateCharacter,
+  deleteCharacter,
+} = require("../controllers/characters");
+
+// GET all characters
+router.get("/", getAllCharacters);
+
+// GET a character by ID
+router.get("/:id", getCharacterById);
+
+// POST a new character
+router.post("/", createCharacter);
+
+// PUT update a character
+router.put("/:id", updateCharacter);
+
+// DELETE a character
+router.delete("/:id", deleteCharacter);
+
+module.exports = router;
